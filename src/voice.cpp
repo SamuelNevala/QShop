d8a715b0ad6a5bf3b2467e3bbb4322000bd43c53
Samuel Nevala <samuel.nevala@gmail.com>
2015-04-05 18:06:44 +0300

    Voice commands

--- src/voice.cpp
+++ src/voice.cpp
@@ -6,9 +6,19 @@
 
 Voice::Voice(QObject *parent)
     : QObject(parent)
-    , m_continious(false)
+    , m_continuous(false)
+    , m_add("lisää lisa lisaa")
+    , m_clean("tyhjennä tyhjenna")
+    , m_end("lopeta")
+    , m_remove("poista")
+    , m_start("monta honda")
 {
     qApp->installEventFilter(this);
+}
+
+bool Voice::continuous() const
+{
+    return m_continuous;
 }
 
 bool Voice::eventFilter(QObject *obj, QEvent *event)
@@ -17,50 +27,69 @@ bool Voice::eventFilter(QObject *obj, QEvent *event)
 
     if (event && event->type() == QEvent::InputMethod) {
         QInputMethodEvent *imEvent = static_cast<QInputMethodEvent *>(event);
-        qDebug()<<obj<<imEvent->commitString();
 
-        if (imEvent->commitString().contains("monta", Qt::CaseInsensitive)) {
-            m_continious = true;
+        qDebug()<<"---- "<<imEvent<<" -----";
+        QString commitString = imEvent->commitString();
+        commitString = commitString.trimmed();
+        m_history.append(commitString);
+
+        if (m_history.length() > 3) {
+            m_history.pop_front();
         }
 
-        if (imEvent->commitString().contains("lopeta", Qt::CaseInsensitive)) {
-            m_continious = false;
+        // Do not block pre-editing.
+        if (!imEvent->preeditString().isEmpty()
+            || imEvent->preeditString().isEmpty() && imEvent->commitString().isEmpty()) {
+            return false;
         }
 
-        if (m_continious && imEvent->commitString().startsWith(" ")) {
-            QString copyStr = imEvent->commitString();
-            if (copyStr.startsWith(" ")) {
-                copyStr = copyStr.remove(0, 1);
-            }
-            if (!copyStr.isEmpty()) {
-                Q_EMIT add(copyStr);
-            }
+        // Attemp to remove duplicate entries
+        if ((m_history.length() == 3
+            && m_history[0] == m_history[2]
+            && m_history[1] == "")
+            || commitString.isEmpty()) {
             return true;
         }
 
-        if (imEvent->commitString().contains("lisää", Qt::CaseInsensitive)) {
-            QString copyStr = imEvent->commitString();
-            copyStr = copyStr.remove("lisää");
-            if (copyStr.startsWith(" ")) {
-                copyStr = copyStr.remove(0, 1);
-            }
-            if (!copyStr.isEmpty()) {
-                Q_EMIT add(copyStr);
-            }
+        if (m_start.contains(commitString, Qt::CaseInsensitive)) {
+            m_continuous = true;
+            Q_EMIT continuousChanged();
             return true;
         }
 
+        if (m_end.contains(commitString, Qt::CaseInsensitive)) {
+            m_continuous = false;
+            Q_EMIT continuousChanged();
+            return true;
+        }
 
-        if (imEvent->commitString().contains("tyhjennä", Qt::CaseInsensitive)) {
+        if (m_clean.contains(commitString, Qt::CaseInsensitive)) {
             Q_EMIT clear();
             return true;
         }
 
-        if (imEvent->commitString().contains("poista", Qt::CaseInsensitive)) {
+        if (m_remove.contains(commitString, Qt::CaseInsensitive)) {
             Q_EMIT remove();
             return true;
         }
 
+        QString start = commitString.split(" ")[0];
+        if (m_add.contains(start, Qt::CaseInsensitive)) {
+            commitString = commitString.remove(start);
+            commitString = commitString.trimmed();
+            qDebug()<<"add "<<commitString;
+            if (!commitString.isEmpty()) {
+                qDebug()<<"emit "<<commitString;
+                Q_EMIT add(commitString);
+            }
+            return true;
+        }
+
+        if (m_continuous && !commitString.isEmpty()) {
+            qDebug()<<"cemit "<<commitString;
+            Q_EMIT add(commitString);
+            return true;
+        }
     }
 
     return QObject::eventFilter(obj, event);
--- src/voice.h
+++ src/voice.h
@@ -2,24 +2,35 @@
 #define VOICECOMMANDS_H
 
 #include <QtCore/QObject>
+#include <QtCore/QStringList>
 
 class Voice : public QObject
 {
     Q_OBJECT
+     Q_PROPERTY(bool continuous READ continuous NOTIFY continuousChanged)
 
 public:
     explicit Voice(QObject *parent = 0);
+    bool continuous() const;
 
 Q_SIGNALS:
     void add(QString text);
     void clear();
     void remove();
+    void continuousChanged();
 
 protected:
     bool eventFilter(QObject *obj, QEvent *event);
 
 private:
-    bool m_continious;
+    bool m_continuous;
+
+    QString m_add;
+    QString m_clean;
+    QString m_end;
+    QString m_remove;
+    QString m_start;
+    QStringList m_history;
 };
 
 #endif // VOICECOMMANDS_H
